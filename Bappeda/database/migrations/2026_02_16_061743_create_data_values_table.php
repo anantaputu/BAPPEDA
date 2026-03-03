5e03d39e49680e0fd9b03b5c3e0c97142613bea5
LepangMbojo <halidrizki54@gmail.com>
2026-03-03 10:58:16 +0800

    ambil yang ini dulu wok

--- Bappeda/database/migrations/2026_02_16_061743_create_data_values_table.php
+++ Bappeda/database/migrations/2026_02_16_061743_create_data_values_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('data_values', function (Blueprint $table) {
-            $table->id('id_value');
+            $table->id('');
        
             $table->unsignedBigInteger('id_data'); 
             
